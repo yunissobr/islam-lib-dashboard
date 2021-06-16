e313d69825cd5d391be8f7c66406407e5982e27f
yunissobr <unisoub@gmail.com>
2021-06-16 12:17:10 +0100

    update

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -5,10 +5,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Redial</title>
+    <title>مكتبة المسلم ~ تسجيل الدخول</title>
 
     <!-- Favicon -->
-    <link rel="icon" href="<?= URLROOT ?>dist/images/favicon.ico" />
+    <!-- <link rel="icon" href="dist/images/favicon.ico" /> -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= URLROOT ?>img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= URLROOT ?>img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= URLROOT ?>img/favicon-16x16.png">
+
+
 
     <!--Plugin CSS-->
     <link href="<?= URLROOT ?>dist/css/plugins.min.css" rel="stylesheet">
